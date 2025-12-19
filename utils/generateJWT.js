7fc9754b930f59d4e501751e5ff48318d06dc286
Pankaj Bepari <pankajbepari7@gmail.com>
2025-12-20 03:21:31 +0600

    add JWT

---
+++ utils/generateJWT.js
@@ -0,0 +1,9 @@
+const jwt = await import("jsonwebtoken");
+
+const generateJWT = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+  });
+};
+
+export default generateJWT;
