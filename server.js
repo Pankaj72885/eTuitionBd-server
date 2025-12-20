be8f044a59fbd8b43aabd560741a439a360f4a2d
Pankaj Bepari <pankajbepari7@gmail.com>
2025-12-20 15:41:14 +0600

    port update

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { initializeFirebase } from "./config/firebaseAdmin.js";
 import { initializeStripe } from "./config/stripe.js";
 dotenv.config();
 
+const port = process.env.PORT || 5000;
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
@@ -18,12 +19,9 @@ mongoose
     initializeStripe();
 
     // Start server
-    if (process.env.NODE_ENV !== "production") {
-      const port = 5000;
-      app.listen(port, () => {
-        console.log(`Server running locally on port ${port}`);
-      });
-    }
+    app.listen(port, "0.0.0.0", () => {
+      console.log(`Server is running on port ${port}`);
+    });
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
